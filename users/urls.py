c064b4cd57a0e0e57454711bc115dd7bfca73aad
Gianik <paoduguran@gmail.com>
2022-10-27 05:18:38 +0800

    remove uncessary imports in posts urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from users.views import RegisterView, HomeView, UpdateProfileView, DashboardView
+from views import RegisterView, HomeView, UpdateProfileView, DashboardView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
