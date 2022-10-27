961cf9fb4c19401adf3f1799522b9ca062b5e796
Gianik <paoduguran@gmail.com>
2022-10-27 22:23:43 +0800

    remove unused imports

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from .models import Post, Comments
-
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import PostForm, CommentForm
 
